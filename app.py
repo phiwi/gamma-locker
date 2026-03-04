1a45b91b58436ee4963fca663133194873bccff7
Philipp Wiesenbach <philipp.wiesenbach@gmail.com>
2026-03-04 21:01:46 +0100

    fix: correct indentation around heatmap block

--- app.py
+++ app.py
@@ -817,7 +817,6 @@ with t0:
                   st.dataframe(heat_df.style.background_gradient(subset=['score'], cmap='viridis'), width='stretch', hide_index=True)
               except ImportError:
                   pass
-            
             display_df['Score'] = display_df['Score'].round(3)
             display_df['Damage'] = (display_df['Damage'] * 100).astype(int)
             display_df['Recoil'] = display_df['Recoil'].round(3)
