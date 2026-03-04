10880053a82a02c6b488018fde05aad421068cb5
Philipp Wiesenbach <philipp.wiesenbach@gmail.com>
2026-03-04 23:57:32 +0100

    chore: remove redundant helper scripts

--- fix_scraper_text4.py
+++
@@ -1,7 +0,0 @@
-with open('scraper.py', 'r') as f:
-    lines = f.readlines()
-for i, l in enumerate(lines):
-    if l.strip() == "except: continue" and i > 180 and i < 195:
-        # It's at line 187
-        lines[i] = "    except: continue\n"
-with open('scraper.py', 'w') as f: f.writelines(lines)
